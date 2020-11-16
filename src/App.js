99f9b5d8d761f4ff63203df977a00b230ca8cc2e
jpbesgen <jpbesgen@berkeley.edu>
2020-11-16 11:25:07 -0800

    added auth to amplify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
+// import Amplify from "aws-amplify";
+// import awsExports from "./aws-exports";
+// Amplify.configure(awsExports);
 
-function App() {
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
+import React, { useEffect, useReducer, useState } from "react";
+import { Button, Col, Container, Form, Row, Table } from "react-bootstrap";
+
+import "./App.css";
+import awsConfig from "./aws-exports";
+
+Amplify.configure(awsConfig);
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row className="mt-3">
+          <Col md={4}>
+            <Form>
+              <Form.Group controlId="formDataName">
+                <Form.Control type="text" name="name" placeholder="Name" />
+              </Form.Group>
+              <Form.Group controlId="formDataDescription">
+                <Form.Control
+                  type="text"
+                  name="description"
+                  placeholder="Description"
+                />
+              </Form.Group>
+              <Form.Group controlId="formDataCity">
+                <Form.Control type="text" name="city" placeholder="City" />
+              </Form.Group>
+              <Button className="float-left">Add New Restaurant</Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
